6e181ca13be9d34e18c31ee87d8182ba7f31cfcf
Markus Wüstenberg <markus@maragu.dk>
2025-04-25 11:11:12 +0200

    Change `http.Authenticate` to only check active user in db
    Otherwise, clients would need to have the exact same `model.User`.
    
    Deleted the `model.User`. Also, added tests for `http.Authenticate`.

--- http/auth.go
+++ http/auth.go
@@ -9,7 +9,7 @@ import (
 	"maragu.dev/gloo/model"
 )
 
-const contextUserKey = contextKey("user")
+const contextUserIDKey = contextKey("userID")
 
 const sessionUserIDKey = "userID"
 
@@ -18,23 +18,27 @@ type sessionDestroyer interface {
 }
 
 type sessionGetter interface {
-	sessionDestroyer
 	Exists(ctx context.Context, key string) bool
 	GetString(ctx context.Context, key string) string
 }
 
-type userGetter interface {
-	GetUser(ctx context.Context, id model.ID) (model.User, error)
+type sessionGetterDestroyer interface {
+	sessionDestroyer
+	sessionGetter
+}
+
+type userActiveChecker interface {
+	IsUserActive(ctx context.Context, id model.ID) (bool, error)
 }
 
 // Authenticate is [Middleware] to authenticate users.
-// After authentication, the user is stored directly in the request context, and can be retrieved using [GetUserFromContext].
-// If there is no session, the middleware does nothing.
-// If there is no user, or the user is inactive, the middleware destroys the session.
-func Authenticate(sg sessionGetter, db userGetter, log *slog.Logger) Middleware {
+// After authentication, the user ID is stored in the request context, and can be retrieved using [GetUserIDFromContext].
+// If there is no session, the middleware does nothing and just calls the next handler.
+// If there is no user (anymore) that's in the session, or the user is inactive, the middleware destroys the session.
+func Authenticate(log *slog.Logger, sg sessionGetterDestroyer, db userActiveChecker) Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// If there is no session, do nothing and return
+			// If there is no session, do nothing and call the next handler
 			if !sg.Exists(r.Context(), sessionUserIDKey) {
 				next.ServeHTTP(w, r)
 				return
@@ -42,7 +46,7 @@ func Authenticate(sg sessionGetter, db userGetter, log *slog.Logger) Middleware 
 
 			// Get the user from the database, and destroy the session if the user is not found
 			userID := model.ID(sg.GetString(r.Context(), sessionUserIDKey))
-			user, err := db.GetUser(r.Context(), userID)
+			active, err := db.IsUserActive(r.Context(), userID)
 			if err != nil {
 				if errors.Is(err, model.ErrorUserNotFound) {
 					if err := sg.Destroy(r.Context()); err != nil {
@@ -62,7 +66,7 @@ func Authenticate(sg sessionGetter, db userGetter, log *slog.Logger) Middleware 
 			}
 
 			// Destroy the session if the user is not active, but continue processing the request
-			if !user.Active {
+			if !active {
 				if err := sg.Destroy(r.Context()); err != nil {
 					log.Info("Error destroying session for inactive user", "error", err, "userID", userID)
 					http.Error(w, "error destroying session after authentication", http.StatusInternalServerError)
@@ -74,18 +78,18 @@ func Authenticate(sg sessionGetter, db userGetter, log *slog.Logger) Middleware 
 			}
 
 			// Store the user directly in the request context instead of having to use the session manager
-			ctx := context.WithValue(r.Context(), contextUserKey, &user)
+			ctx := context.WithValue(r.Context(), contextUserIDKey, &userID)
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
 	}
 }
 
-// GetUserFromContext, which may be nil if the user is not authenticated.
-func GetUserFromContext(ctx context.Context) *model.User {
-	user := ctx.Value(contextUserKey)
-	if user == nil {
+// GetUserIDFromContext, which may be nil if the user is not authenticated.
+func GetUserIDFromContext(ctx context.Context) *model.ID {
+	id := ctx.Value(contextUserIDKey)
+	if id == nil {
 		return nil
 	}
 
-	return user.(*model.User)
+	return id.(*model.ID)
 }
