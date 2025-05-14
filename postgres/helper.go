5774ed6d0eb43346e91380e5af36c3735f3e55c8
Markus Wüstenberg <markus@maragu.dk>
2025-05-14 13:35:14 +0200

    Add `postgres.Helper` and `postgrestest` package

---
+++ postgres/helper.go
@@ -0,0 +1,136 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"log/slog"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+	"maragu.dev/errors"
+)
+
+type Helper struct {
+	DB                    *sqlx.DB
+	connectionMaxIdleTime time.Duration
+	connectionMaxLifetime time.Duration
+	log                   *slog.Logger
+	maxIdleConnections    int
+	maxOpenConnections    int
+	url                   string
+}
+
+type NewHelperOptions struct {
+	ConnectionMaxIdleTime time.Duration
+	ConnectionMaxLifetime time.Duration
+	Log                   *slog.Logger
+	MaxIdleConnections    int
+	MaxOpenConnections    int
+	URL                   string
+}
+
+// NewHelper with the given options.
+// If no logger is provided, logs are discarded.
+func NewHelper(opts NewHelperOptions) *Helper {
+	if opts.Log == nil {
+		opts.Log = slog.New(slog.DiscardHandler)
+	}
+
+	return &Helper{
+		log: opts.Log,
+		url: opts.URL,
+	}
+}
+
+func (h *Helper) Connect(ctx context.Context) error {
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	h.log.Info("Connecting to database", "url", h.url)
+
+	var err error
+	h.DB, err = sqlx.ConnectContext(ctx, "pgx", h.url)
+	if err != nil {
+		return err
+	}
+
+	h.log.Debug("Setting connection pool options",
+		"max open connections", h.maxOpenConnections,
+		"max idle connections", h.maxIdleConnections,
+		"connection max lifetime", h.connectionMaxLifetime,
+		"connection max idle time", h.connectionMaxIdleTime)
+	h.DB.SetMaxOpenConns(h.maxOpenConnections)
+	h.DB.SetMaxIdleConns(h.maxIdleConnections)
+	h.DB.SetConnMaxLifetime(h.connectionMaxLifetime)
+	h.DB.SetConnMaxIdleTime(h.connectionMaxIdleTime)
+
+	return nil
+}
+
+// InTransaction runs callback in a transaction, and makes sure to handle rollbacks, commits etc.
+func (h *Helper) InTransaction(ctx context.Context, cb func(tx *Tx) error) (err error) {
+	tx, err := h.DB.BeginTxx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
+	if err != nil {
+		return errors.Wrap(err, "error beginning transaction")
+	}
+	defer func() {
+		if rec := recover(); rec != nil {
+			err = rollback(tx, errors.Newf("panic: %v", rec))
+		}
+	}()
+	if err := cb(&Tx{Tx: tx}); err != nil {
+		return rollback(tx, err)
+	}
+	if err := tx.Commit(); err != nil {
+		return errors.Wrap(err, "error committing transaction")
+	}
+
+	return nil
+}
+
+// rollback a transaction, handling both the original error and any transaction rollback errors.
+func rollback(tx *sqlx.Tx, err error) error {
+	if txErr := tx.Rollback(); txErr != nil {
+		return errors.Wrap(err, "error rolling back transaction after error (transaction error: %v), original error", txErr)
+	}
+	return err
+}
+
+func (h *Helper) Ping(ctx context.Context) error {
+	return h.InTransaction(ctx, func(tx *Tx) error {
+		return tx.Exec(ctx, `select 1`)
+	})
+}
+
+func (h *Helper) Select(ctx context.Context, dest any, query string, args ...any) error {
+	return h.DB.SelectContext(ctx, dest, query, args...)
+}
+
+func (h *Helper) Get(ctx context.Context, dest any, query string, args ...any) error {
+	return h.DB.GetContext(ctx, dest, query, args...)
+}
+
+func (h *Helper) Exec(ctx context.Context, query string, args ...any) error {
+	_, err := h.DB.ExecContext(ctx, query, args...)
+	return err
+}
+
+type Tx struct {
+	Tx *sqlx.Tx
+}
+
+func (t *Tx) Select(ctx context.Context, dest any, query string, args ...any) error {
+	return t.Tx.SelectContext(ctx, dest, query, args...)
+}
+
+func (t *Tx) Get(ctx context.Context, dest any, query string, args ...any) error {
+	return t.Tx.GetContext(ctx, dest, query, args...)
+}
+
+func (t *Tx) Exec(ctx context.Context, query string, args ...any) error {
+	_, err := t.Tx.ExecContext(ctx, query, args...)
+	return err
+}
+
+var ErrNoRows = sql.ErrNoRows
---
+++ postgres/migrate_test.go
@@ -0,0 +1,31 @@
+package postgres_test
+
+import (
+	"testing"
+
+	"maragu.dev/is"
+
+	"maragu.dev/gloo/postgrestest"
+)
+
+func TestHelper_Migrate(t *testing.T) {
+	t.Run("can migrate down and back up", func(t *testing.T) {
+		h := postgrestest.NewHelper(t)
+
+		err := h.MigrateDown(t.Context())
+		is.NotError(t, err)
+
+		err = h.MigrateUp(t.Context())
+		is.NotError(t, err)
+
+		var version string
+		err = h.Get(t.Context(), &version, `select version from migrations`)
+		is.NotError(t, err)
+		is.True(t, len(version) > 0)
+
+		var id string
+		err = h.Get(t.Context(), &id, `select id from gloo`)
+		is.NotError(t, err)
+		is.Equal(t, id, "1")
+	})
+}
