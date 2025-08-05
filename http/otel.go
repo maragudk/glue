5ba2294ef944beeab9abf904b621eeae4430eff1
Markus Wüstenberg <markus@maragu.dk>
2025-08-05 13:36:55 +0200

    Add "main" span attribute to all HTTP requests

--- http/otel.go
+++ http/otel.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel/attribute"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 	"go.opentelemetry.io/otel/trace"
 )
@@ -18,6 +19,10 @@ func OpenTelemetry(next http.Handler) http.Handler {
 			span := trace.SpanFromContext(r.Context())
 			span.SetName(r.Method + " " + routePattern)
 			span.SetAttributes(semconv.HTTPRoute(routePattern))
+
+			// The idea of a "main" span is from "A Practitioner's Guide to Wide Events":
+			// https://jeremymorrell.dev/blog/a-practitioners-guide-to-wide-events/#:~:text=A%20convention%20to%20filter%20out%20everything%20else
+			span.SetAttributes(attribute.Bool("main", true))
 		}),
 		"", // Setting the name here doesn't matter, it's done on the span above
 	)
