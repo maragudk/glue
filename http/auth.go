a428b48b4c0cb29de13bc29f75cc60492d8ac072
Markus Wüstenberg <markus@maragu.dk>
2025-08-06 13:52:18 +0200

    Add HTTP middleware tracing

--- http/auth.go
+++ http/auth.go
@@ -8,6 +8,7 @@ import (
 	"net/url"
 	"slices"
 
+	"go.opentelemetry.io/otel"
 	g "maragu.dev/gomponents"
 
 	"maragu.dev/glue/html"
@@ -42,20 +43,26 @@ type userActiveChecker interface {
 // If there is no session, the middleware does nothing and just calls the next handler.
 // If there is no user (anymore) but the ID is in the session, or the user is inactive, the middleware destroys the session and calls the next handler.
 func Authenticate(log *slog.Logger, sgd sessionGetterDestroyer, uac userActiveChecker) Middleware {
+	tracer := otel.Tracer("maragu.dev/glue/http")
+
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx, span := tracer.Start(r.Context(), "Authenticate")
+			defer span.End()
+			r = r.WithContext(ctx)
+
 			// If there is no session, do nothing and call the next handler
-			if !sgd.Exists(r.Context(), SessionUserIDKey) {
+			if !sgd.Exists(ctx, SessionUserIDKey) {
 				next.ServeHTTP(w, r)
 				return
 			}
 
 			// Get the user from the database, and destroy the session if the user is not found
-			userID := model.UserID(sgd.GetString(r.Context(), SessionUserIDKey))
-			active, err := uac.IsUserActive(r.Context(), userID)
+			userID := model.UserID(sgd.GetString(ctx, SessionUserIDKey))
+			active, err := uac.IsUserActive(ctx, userID)
 			if err != nil {
 				if errors.Is(err, model.ErrorUserNotFound) {
-					if err := sgd.Destroy(r.Context()); err != nil {
+					if err := sgd.Destroy(ctx); err != nil {
 						log.Info("Error destroying session for nonexistent user", "error", err, "userID", userID)
 						http.Error(w, "error destroying session after authentication", http.StatusInternalServerError)
 						return
@@ -73,7 +80,7 @@ func Authenticate(log *slog.Logger, sgd sessionGetterDestroyer, uac userActiveCh
 
 			// Destroy the session if the user is not active, but continue processing the request
 			if !active {
-				if err := sgd.Destroy(r.Context()); err != nil {
+				if err := sgd.Destroy(ctx); err != nil {
 					log.Info("Error destroying session for inactive user", "error", err, "userID", userID)
 					http.Error(w, "error destroying session after authentication", http.StatusInternalServerError)
 					return
@@ -84,7 +91,7 @@ func Authenticate(log *slog.Logger, sgd sessionGetterDestroyer, uac userActiveCh
 			}
 
 			// Store the user directly in the request context instead of having to use the session manager
-			ctx := context.WithValue(r.Context(), contextUserIDKey, &userID)
+			ctx = context.WithValue(ctx, contextUserIDKey, &userID)
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
 	}
@@ -101,16 +108,22 @@ func GetUserIDFromContext(ctx context.Context) *model.UserID {
 }
 
 func Authorize(log *slog.Logger, pg permissionsGetter, requiredPermissions ...model.Permission) Middleware {
+	tracer := otel.Tracer("maragu.dev/glue/http")
+
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			userID := GetUserIDFromContext(r.Context())
+			ctx, span := tracer.Start(r.Context(), "Authorize")
+			defer span.End()
+			r = r.WithContext(ctx)
+
+			userID := GetUserIDFromContext(ctx)
 
 			if userID == nil {
 				http.Redirect(w, r, "/login?redirect="+url.QueryEscape(r.URL.Path), http.StatusTemporaryRedirect)
 				return
 			}
 
-			permissions, err := pg.GetPermissions(r.Context(), *userID)
+			permissions, err := pg.GetPermissions(ctx, *userID)
 			if err != nil {
 				log.Info("Error getting permissions", "error", err, "userID", userID)
 				http.Error(w, "error getting permissions", http.StatusInternalServerError)
@@ -140,23 +153,29 @@ type permissionsGetter interface {
 }
 
 func SavePermissionsInContext(log *slog.Logger, pg permissionsGetter) Middleware {
+	tracer := otel.Tracer("maragu.dev/glue/http")
+
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			userID := GetUserIDFromContext(r.Context())
+			ctx, span := tracer.Start(r.Context(), "SavePermissionsInContext")
+			defer span.End()
+			r = r.WithContext(ctx)
+
+			userID := GetUserIDFromContext(ctx)
 
 			if userID == nil {
 				next.ServeHTTP(w, r)
 				return
 			}
 
-			permissions, err := pg.GetPermissions(r.Context(), *userID)
+			permissions, err := pg.GetPermissions(ctx, *userID)
 			if err != nil {
 				log.Error("Error getting permissions", "error", err, "userID", userID)
 				http.Error(w, "error getting permissions", http.StatusInternalServerError)
 				return
 			}
 
-			ctx := context.WithValue(r.Context(), contextPermissionsKey, permissions)
+			ctx = context.WithValue(ctx, contextPermissionsKey, permissions)
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
 	}
