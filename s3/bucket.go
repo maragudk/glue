8a00038a758fc3fb310f88ccddf39bcffa5014a4
Markus Wüstenberg <markus@maragu.dk>
2025-08-06 13:48:22 +0200

    Add tracing to `s3.Bucket` (#43)
    This also fixes a bug where `s3.Bucket.Get` didn't return errors
    correctly.

--- s3/bucket.go
+++ s3/bucket.go
@@ -3,16 +3,25 @@ package s3
 
 import (
 	"context"
+	"errors"
 	"io"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	semconv "go.opentelemetry.io/otel/semconv/v1.34.0"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type Bucket struct {
-	Client *s3.Client
-	name   string
+	Client     *s3.Client
+	attributes []attribute.KeyValue
+	name       string
+	tracer     trace.Tracer
 }
 
 type NewBucketOptions struct {
@@ -33,42 +42,82 @@ func NewBucket(opts NewBucketOptions) *Bucket {
 
 	return &Bucket{
 		Client: client,
+		attributes: []attribute.KeyValue{
+			semconv.AWSS3Bucket(opts.Name),
+			semconv.CloudRegion(opts.Config.Region),
+		},
 		name:   opts.Name,
+		tracer: otel.Tracer("maragu.dev/glue/s3"),
 	}
 }
 
 // Put an object under key with the given contentType.
 func (b *Bucket) Put(ctx context.Context, key, contentType string, body io.ReadSeeker) error {
+	ctx, span := b.operationTracerStart(ctx, "PUT", key)
+	defer span.End()
+
 	_, err := b.Client.PutObject(ctx, &s3.PutObjectInput{
 		Bucket:      &b.name,
 		Key:         &key,
 		Body:        body,
 		ContentType: &contentType,
 	})
-	return err
+	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, "put failed")
+		return err
+	}
+
+	span.SetStatus(codes.Ok, "")
+
+	return nil
 }
 
 // Get an object under key.
 // If there is nothing there, returns nil and no error.
 func (b *Bucket) Get(ctx context.Context, key string) (io.ReadCloser, error) {
+	ctx, span := b.operationTracerStart(ctx, "GET", key)
+	defer span.End()
+
 	getObjectOutput, err := b.Client.GetObject(ctx, &s3.GetObjectInput{
 		Bucket: &b.name,
 		Key:    &key,
 	})
-	if getObjectOutput == nil {
-		return nil, nil
+	if err != nil {
+		var noSuchKeyError *types.NoSuchKey
+		if errors.As(err, &noSuchKeyError) {
+			span.SetStatus(codes.Ok, "")
+			return nil, nil
+		}
+		span.RecordError(err)
+		span.SetStatus(codes.Error, "get failed")
+		return nil, err
 	}
-	return getObjectOutput.Body, err
+
+	span.SetStatus(codes.Ok, "")
+
+	return getObjectOutput.Body, nil
 }
 
 // Delete an object under key.
 // Deleting where nothing exists does nothing and returns no error.
 func (b *Bucket) Delete(ctx context.Context, key string) error {
+	ctx, span := b.operationTracerStart(ctx, "DELETE", key)
+	defer span.End()
+
 	_, err := b.Client.DeleteObject(ctx, &s3.DeleteObjectInput{
 		Bucket: &b.name,
 		Key:    &key,
 	})
-	return err
+	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, "delete failed")
+		return err
+	}
+
+	span.SetStatus(codes.Ok, "")
+
+	return nil
 }
 
 func (b *Bucket) GetPresignedURL(ctx context.Context, key string, expires time.Duration) (string, error) {
@@ -85,17 +134,38 @@ func (b *Bucket) GetPresignedURL(ctx context.Context, key string, expires time.D
 }
 
 func (b *Bucket) List(ctx context.Context, prefix string, maxKeys int) ([]string, error) {
+	ctx, span := b.operationTracerStart(ctx, "LIST", prefix)
+	defer span.End()
+
 	listObjectsOutput, err := b.Client.ListObjectsV2(ctx, &s3.ListObjectsV2Input{
 		Bucket:  &b.name,
 		MaxKeys: aws.Int32(int32(maxKeys)),
 		Prefix:  &prefix,
 	})
 	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, "list failed")
 		return nil, err
 	}
+
 	var keys []string
 	for _, object := range listObjectsOutput.Contents {
 		keys = append(keys, *object.Key)
 	}
+
+	span.SetStatus(codes.Ok, "")
+
 	return keys, nil
+}
+
+func (b *Bucket) operationTracerStart(ctx context.Context, operation, key string, opts ...trace.SpanStartOption) (context.Context, trace.Span) {
+	allOpts := []trace.SpanStartOption{
+		trace.WithSpanKind(trace.SpanKindClient),
+		trace.WithAttributes(b.attributes...),
+		trace.WithAttributes(
+			semconv.AWSS3Key(key),
+		),
+	}
+	allOpts = append(allOpts, opts...)
+	return b.tracer.Start(ctx, operation, allOpts...)
 }
